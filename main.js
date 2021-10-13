cddbd822ad375da9676ba50dfbc15bc4b059f39b
Dennys <keta548@vivaldi.net>
2021-10-13 19:51:10 +0300

    Update main.js
    Сложнее оказался гитхаб чем код

---
+++ main.js
@@ -0,0 +1,61 @@
+// task 1
+let player1 = {
+	name: 'Scorpion',
+	hp: 50,
+	img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+	weapons: ['fist', 'leg', 'poisonspit'],
+	attack: function () {
+		console.log(player1.name + 'Fight...')
+	},
+}
+let player2 = {
+	name: 'Subzero',
+	hp: 80,
+	img: 'http://reactmarathon-api.herokuapp.com/assets/subzero.gif',
+	weapons: ['fist', 'leg', 'frostball'],
+	attack: function () {
+		console.log(player2.name + 'Fight...')
+	},
+}
+
+// task 2
+function createPlayer(player, identificator) {
+	//player
+	let $player = document.createElement('div');
+	$player.classList.add(player);
+
+	let $progressbar = document.createElement('div');
+	$progressbar.classList.add('progressbar');
+
+	let $life = document.createElement('div');
+	$life.classList.add('life');
+	$life.style.width = '100%';
+
+	let $name = document.createElement('div');
+	$name.classList.add('name');
+	$name.innerText = identificator.name;
+
+	let $character = document.createElement('div');
+	$character.classList.add('character');
+	let $characterImg = document.createElement('img');
+	$characterImg.src = identificator.img;
+
+	$player.appendChild($progressbar);
+	$player.appendChild($character);
+
+	$progressbar.appendChild($life);
+	$progressbar.appendChild($name);
+
+	$character.appendChild($characterImg);
+
+	const $arenas = document.querySelector('.arenas')
+	$arenas.appendChild($player);
+
+}
+
+//task 3
+
+createPlayer('player1', player1);
+createPlayer('player2', player2);
+
+
