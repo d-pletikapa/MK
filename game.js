3286e11fbfacfff15f510c1a7b499e9baa79b65a
Dennys <keta548@vivaldi.net>
2021-10-28 23:02:31 +0300

    Убрал class PLayer в player.js

--- game.js
+++ game.js
@@ -1,39 +1,8 @@
 import { logs } from './logs.js';
-
+import { Player } from './player.js';
 export class Game {
 	//constructor(props) {	}
 	start = () => {
-
-		class Player {
-			constructor(props) {
-				//console.log(props);
-				this.player = props.player;
-				this.name = props.name;
-				this.hp = props.hp;
-				this.img = props.img;
-			};
-
-			elHP = () => {
-				return document.querySelector(`.player${this.player} .life`);
-			}
-
-			changeHP = (getRandom) => {
-				//player.hp -= Math.floor(Math.random() * 20 + 1);
-				this.hp -= getRandom;
-				if (this.hp < 0) { this.hp = 0; }
-				//console.log(this.name + ' ' + this.hp + ' hp ' + 'left');
-				return this.hp;
-			};
-
-			renderHP = () => {
-				this.elHP().style.width = this.hp + '%';
-			};
-
-			attack = () => {
-				console.log(this.name + 'Fight...');
-			};
-		};
-
 		const $arenas = document.querySelector('.arenas');
 		const $formFight = document.querySelector('.control');
 		const $chat = document.querySelector('.chat');
